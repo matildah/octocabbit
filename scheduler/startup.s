bb81c1f9110a2c77bf4c2325c28f8ba0f8d66558
Kia <2592730+superbaud@users.noreply.github.com>
2014-06-25 04:32:15 -0700

    commented up the shit but somehow it's buggy

--- scheduler/startup.s
+++ scheduler/startup.s
@@ -1,6 +1,32 @@
+/*
+ * this is the entry point
+ *
+ * asm_entry is loaded at 0x10000 and is the first bit of our code that gets
+ * executed. We copy the exception vectors to the exception vector base 
+ * location and set up stack pointers for all the modes we will use.
+ * We then call the c entry point, c_entry.
+ */
+
+/*
+ * Copyright (c) 2014 Matilda <sadieperkins@riseup.net>
+ * 
+ * Permission to use, copy, modify, and/or distribute this software for any 
+ * purpose with or without fee is hereby granted, provided that the above 
+ * copyright notice and this permission notice appear in all copies.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES 
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF 
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY 
+ * SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER 
+ * RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, 
+ * NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE 
+ * USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
 .section .text
 .global asm_entry
 asm_entry:
+    /*
     mov r0, #0x0
     ldr r1, =_vectors
     ldr r3, =_vectors_end
@@ -10,10 +36,10 @@ keep_honking:
     add r0, r0, #0x4
     add r1, r1, #0x4
     cmp r1, r3
-    bne keep_honking
+    bne keep_honking    /* I'M RELOADING */
 
-    LDR sp, =stack_top
-    BL c_entry
+    ldr sp, =stack_top
+    bl c_entry
 
 undef_inst:
 swi:
@@ -21,19 +47,19 @@ abort_prefetch:
 abort_data:
 irq:
 fiq:
-    B .
+    b .
 
 
 
 _vectors:
-    LDR pc, _reset
-    LDR pc, _undef_inst
-    LDR pc, _swi
-    LDR pc, _abort_prefetch
-    LDR pc, _abort_data
+    ldr pc, _reset
+    ldr pc, _undef_inst
+    ldr pc, _swi
+    ldr pc, _abort_prefetch
+    ldr pc, _abort_data
     B .                         /* padding */
-    LDR pc, _irq
-    LDR pc, _fiq
+    ldr pc, _irq
+    ldr pc, _fiq
 
 _reset: .word asm_entry
 _undef_inst: .word undef_inst

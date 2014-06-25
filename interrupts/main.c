2d803281648f2113597168022306eafd68798def
Kia <2592730+superbaud@users.noreply.github.com>
2014-06-25 05:43:41 -0700

    take out kputs

--- interrupts/main.c
+++ interrupts/main.c
@@ -1,13 +1,10 @@
 #include <stdint.h>
-
-volatile uint32_t * my_uart = 0x101f1000 ;
+#include <uart.h>
 void c_entry() 
 {
     char *foo = "hello world!\n";
-    while (*foo != 0) {
-        *my_uart = *foo;
-        foo++;
-        asm("svc 0x0");
-    }
+    kputs(foo);
 
 }
+
+

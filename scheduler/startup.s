a3ba48084db9ae6ca2f2d252905ee4069fce0a1a
Matilda <sadieperkins@riseup.net>
2014-06-25 03:07:20 -0700

    we made a table of vectors!

--- scheduler/startup.s
+++ scheduler/startup.s
@@ -1,3 +1,28 @@
+.section vectors
+.global _vectors
+_vectors:
+    LDR pc, _reset
+    LDR pc, _undef_inst
+    LDR pc, _swi
+    LDR pc, _abort_prefetch
+    LDR pc, _abort_data
+    B .                         /* padding */
+    LDR pc, _irq
+    LDR pc, _fiq
+
+_reset: .word _asm_entry
+_undef_inst: .word undef_inst
+_swi: .word swi
+_abort_prefetch: .word abort_prefetch
+_abort_data: .word abort_data
+_irq: .word irq
+_fiq: .word fiq
+
+
+
+
+
+
 .section text
 .global _asm_entry
 _asm_entry:

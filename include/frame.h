d749ad69be01a6bd50a86ee96b3d786759798933
Matilda <sadieperkins@riseup.net>
2014-06-28 18:45:49 -0700

    removed the struct trapframe i got from the openbsd frame.h, i'll make my own

--- include/frame.h
+++ include/frame.h
@@ -14,111 +14,31 @@
  * USE OR PERFORMANCE OF THIS SOFTWARE.
  */
 
-/*
- * i have taken taken things from OpenBSD's sys/arch/arm/include/frame.h:
- * PUSHFRAME and PULLFRAME
- * struct trapframe
- * The copyright notice and license for that file is below.
- */
-
-/*
- * Copyright (c) 1994-1997 Mark Brinicombe.
- * Copyright (c) 1994 Brini.
- * All rights reserved.
- *
- * This code is derived from software written for Brini by Mark Brinicombe
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- * 3. All advertising materials mentioning features or use of this software
- *    must display the following acknowledgement:
- * This product includes software developed by Brini.
- * 4. The name of the company nor the name of the author may be used to
- *    endorse or promote products derived from this software without specific
- *    prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY BRINI ``AS IS'' AND ANY EXPRESS OR IMPLIED
- * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
- * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
- * IN NO EVENT SHALL BRINI OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
- * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
- * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
- * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
- * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
- * SUCH DAMAGE.
- *
- * RiscBSD kernel project
- *
- * frame.h
- *
- * Stack frames structures
- *
- * Created      : 30/09/94
- */
-
 #ifndef _FRAME_H_
 #define _FRAME_H_
 
 #include <ktypes.h>
 
 struct trapframe {
-    register_t spsr;    /* 0  */
-    register_t r0;      /* 1  */
-    register_t r1;      /* 2  */
-    register_t r2;      /* 3  */
-    register_t r3;      /* 4  */
-    register_t r4;      /* 5  */
-    register_t r5;      /* 6  */
-    register_t r6;      /* 7  */
-    register_t r7;      /* 8  */
-    register_t r8;      /* 9  */
-    register_t r9;      /* 10 */
-    register_t r10;     /* 11 */
-    register_t r11;     /* 12 */
-    register_t r12;     /* 13 */
-    register_t cur_sp;  /* 14 */ /* r13 from where we were interrupted */
-    register_t cur_lr;  /* 15 */ /* r14 from where we were interrupted */
-    register_t svc_sp;  /* 16 */ /* r13 from supervisor mode */
-    register_t svc_lr;  /* 17 */ /* r14 from supervisor mode */
-    register_t pc;      /* 18 */
+    register_t r0;
+    register_t r1;
+    register_t r2;
+    register_t r3;
+    register_t r4;
+    register_t r5;
+    register_t r6;
+    register_t r7;
+    register_t r8;
+    register_t r9;
+    register_t r10;
+    register_t r11;
+    register_t r12;
+    register_t cur_sp;
+    register_t cur_lr;
+    register_t svc_sp;
+    register_t svc_lr;
+    register_t pc;
 };
-
-
-/* the following macros DO NOT involve any mode changes */
-
-/*
- * PUSHFRAME - macro to push a trap frame on the stack in the current mode
- * Since the current mode is used, the SVC lr field is not defined.
- */
-
-#define PUSHFRAME                                                            \
-    str    lr, [sp, #-4]!;    /* Push the return address */                  \
-    sub    sp, sp, #(4*17);   /* Adjust the stack pointer */                 \
-    stmia  sp, {r0-r14}^;     /* Push the user mode registers */             \
-    mov    r0, r0;            /* NOP for previous instruction */             \
-    mrs    r0, spsr_all;      /* Put the SPSR on the stack */                \
-    str    r0, [sp, #-4]!
-
-/*
- * PULLFRAME - macro to pull a trap frame from the stack in the current mode
- * Since the current mode is used, the SVC lr field is ignored.
- */
-
-#define PULLFRAME                                                            \
-    ldr    r0, [sp], #0x0004;   /* Get the SPSR from stack */                \
-    msr    spsr_all, r0;                                                     \
-    ldmia  sp, {r0-r14}^;       /* Restore registers (usr mode) */           \
-    mov    r0, r0;              /* NOP for previous instruction */           \
-    add    sp, sp, #(4*17);     /* Adjust the stack pointer */               \
-    ldr    lr, [sp], #0x0004    /* Pull the return address */
 
 
 

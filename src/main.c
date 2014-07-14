cf8ecb69dfa49a204230635aad9e308085a874be
Matilda <sadieperkins@riseup.net>
2014-07-14 06:19:11 -0700

    explained what svc does in here

--- src/main.c
+++ src/main.c
@@ -4,10 +4,10 @@ void c_entry()
 {
     uint32_t forkres = 0xfeedface;
     kprintf("entered c_entry\n");
-    asm volatile("svc 0x0\n\t mov %0, r0" : "=r" (forkres));
+    asm volatile("svc 0x0\n\t mov %0, r0" : "=r" (forkres)); /* this acts like fork */
     if (forkres == 1) { /* parent */
         while (1) {
-            asm volatile("svc 0x0");
+            asm volatile("svc 0x0"); /* but further calls just yield control to the other thread */
             kprintf("PARENT!\n");
         }
     } else {
